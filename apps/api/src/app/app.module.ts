3e3c01f91c6d4da69b797c7576af30075c1f4004
Szwajowy <krzycza96@gmail.com>
2022-11-02 21:29:21 +0100

    Import Devices library into API

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DevicesController, DevicesService } from '@desk-clock/devices';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
@@ -5,7 +6,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, DevicesController],
+  providers: [AppService, DevicesService],
 })
 export class AppModule {}
