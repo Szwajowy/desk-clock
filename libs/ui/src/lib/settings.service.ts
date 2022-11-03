97cd55dcd8596f70f05fc9a846a1a1f89d190eb9
Szwajowy <krzycza96@gmail.com>
2022-11-03 11:11:16 +0100

    Get settings from backend

--- libs/ui/src/lib/settings.service.ts
+++ libs/ui/src/lib/settings.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Settings } from '@prisma/client';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SettingsService {
+  private API_URL = 'http://localhost:3333/api';
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) {}
+
+  public getProducts(): Observable<Settings[]> {
+    {
+      return this.http.get<Settings[]>(`${this.API_URL}/settings`);
+    }
+  }
 }
