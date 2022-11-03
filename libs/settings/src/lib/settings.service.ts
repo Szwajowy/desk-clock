a667dbf4675ad200e5acfdaf38941ce638b91ed7
Szwajowy <krzycza96@gmail.com>
2022-11-03 11:02:05 +0100

    Create get settings endpoint

--- libs/settings/src/lib/settings.service.ts
+++ libs/settings/src/lib/settings.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient, Settings } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 @Injectable()
-export class SettingsService {}
+export class SettingsService {
+  public getSettings(): Promise<Settings[]> {
+    return prisma.settings.findMany();
+  }
+}
